f1bafb5e33c91179331985b6c4a330add98a9720
Dustin Armstrong <darmstrong@pizzakingstores.com>
2017-04-29 16:17:28 -0400

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Pizza King - Ring The King"
+    assert_select "title", "Pizza King - Ring The King"
   end
 
   test "should get help" do
